41df5eb2a9a399ec8a008b0c4613be0c22f4229e
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-04-22 02:35:21 +0300

    initial commit

--- flatiron-bank/src/components/AddTransactionForm.js
+++ flatiron-bank/src/components/AddTransactionForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { addTransaction } from "./Api";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddTransactionForm = ({ onAdd }) => {
   const [formData, setFormData] = useState({
-    date: "",
+    date: new Date(),
     description: "",
     category: "",
     amount: "",
@@ -18,66 +20,67 @@ const AddTransactionForm = ({ onAdd }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-    const response = await addTransaction(formData);
-    onAdd(response);
-    setFormData({
-    date: "",
-    description: "",
-    category: "",
-    amount: "",
-    });
+      const response = await addTransaction(formData);
+      onAdd(response);
+      setFormData({
+        date: new Date(),
+        description: "",
+        category: "",
+        amount: "",
+      });
     } catch (error) {
-    console.error(error);
+      console.error(error);
     }
-    };
-    
-    return (
+  };
+
+  return (
     <Form onSubmit={handleSubmit}>
-    <Form.Group controlId="formDate">
-    <Form.Label>Date</Form.Label>
-    <Form.Control
-    type="date"
-    name="date"
-    value={formData.date}
-    onChange={handleInputChange}
-    required
-    />
-    </Form.Group>
-    <Form.Group controlId="formDescription">
-    <Form.Label>Description</Form.Label>
-    <Form.Control
-    type="text"
-    name="description"
-    value={formData.description}
-    onChange={handleInputChange}
-    required
-    />
-    </Form.Group>
-    <Form.Group controlId="formCategory">
-    <Form.Label>Category</Form.Label>
-    <Form.Control
-    type="text"
-    name="category"
-    value={formData.category}
-    onChange={handleInputChange}
-    required
-    />
-    </Form.Group>
-    <Form.Group controlId="formAmount">
-    <Form.Label>Amount</Form.Label>
-    <Form.Control
-    type="number"
-    name="amount"
-    value={formData.amount}
-    onChange={handleInputChange}
-    required
-    />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-    Add Transaction
-    </Button>
+      <Form.Group controlId="formDate">
+        <Form.Label>Date</Form.Label>
+        <DatePicker
+          selected={formData.date}
+          onChange={(date) =>
+            setFormData({ ...formData, date: date.toISOString() })
+          }
+          dateFormat="yyyy-MM-dd"
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formCategory">
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          type="text"
+          name="category"
+          value={formData.category}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formAmount">
+        <Form.Label>Amount</Form.Label>
+        <Form.Control
+          type="number"
+          name="amount"
+          value={formData.amount}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Add Transaction
+      </Button>
     </Form>
-    );
-    };
-    
-    export default AddTransactionForm;
+  );
+};
+
+export default AddTransactionForm;
