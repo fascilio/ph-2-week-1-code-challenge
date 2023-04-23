29bc5ae983fa7037bc015799472ce2a21e2023c2
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-04-23 23:03:11 +0300

    added a README.md file

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # ph-2-week-1-code-challenge
+
+## Code Structure
+## App.js
+This is the main component of the application that renders the other 
+components. It uses the useState and useEffect hooks to manage state 
+and handle API requests. The transactions state variable stores all 
+the transactions fetched from the API, while the filteredTransactions 
+state variable is used to store the transactions that match the 
+search term entered by the user. The searchTerm state variable stores 
+the search term entered by the user.
+
+The useEffect hook is used to fetch the transactions from the API 
+when the component mounts. The handleAddTransaction function is used 
+to add a new transaction to the transactions and filteredTransactions 
+state variables. The handleSearch function is used to filter the 
+transactions based on the search term entered by the user.
+
+## Api.js
+This file contains two functions: getTransactions and addTransaction. 
+These functions are used to fetch transactions from the API and add 
+new transactions to the API, respectively. The axios library is used 
+to make HTTP requests to the API.
+
+## TransactionList.js
+This component is used to render the list of transactions. It 
+receives the filteredTransactions state variable as a prop and maps 
+over it to create a table row for each transaction.
+
+## AddTransactionForm.js
+This component is used to render the form for adding a new 
+transaction. It uses the useState hook to manage form data and the 
+addTransaction function from the Api.js file to add the new transaction to the API.
+
+## Home.js
+This component is a simple header that is displayed at the top of the page.
+
+## Rendering
+The application renders a table of transactions that can be searched 
+by description. Users can add new transactions using the form at the 
+top of the page. The home page displays a simple header with the name of the bank.
--- flatiron-bank/src/components/App.js
+++ flatiron-bank/src/components/App.js
@@ -1,8 +1,78 @@
+// import React, { useState, useEffect } from "react";
+// import Home from "./Home";
+// import { Container, Row, Col } from "react-bootstrap";
+// import TransactionList from "./TransactionList";
+// import AddTransactionForm from "./AddTransactionForm";
+// import { getTransactions } from "./Api";
+
+// const App = () => {
+//   const [transactions, setTransactions] = useState([]);
+//   const [filteredTransactions, setFilteredTransactions] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     const fetchTransactions = async () => {
+//       try {
+//         const response = await getTransactions();
+//         setTransactions(response);
+//         setFilteredTransactions(response);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchTransactions();
+//   }, []);
+
+//   const handleAddTransaction = (transaction) => {
+//     setTransactions([...transactions, transaction]);
+//     setFilteredTransactions([...filteredTransactions, transaction]);
+//   };
+
+//   const handleSearch = (event) => {
+//     setSearchTerm(event.target.value);
+//     const filtered = transactions.filter((transaction) =>
+//       transaction.description.toLowerCase().includes(event.target.value.toLowerCase())
+//     );
+//     setFilteredTransactions(filtered);
+//   };
+
+//   return (
+//     <Container className="my-5">
+//       <Row>
+//         <Col>
+//           <h1 className="text-center">Bank Transactions</h1>
+//         </Col>
+//       </Row>
+//       <Row>
+//         <Col>
+//           <AddTransactionForm onAdd={handleAddTransaction} />
+//         </Col>
+//       </Row>
+//       <Row>
+//         <Col>
+//           <input
+//             type="text"
+//             className="form-control mb-3"
+//             placeholder="Search transactions..."
+//             value={searchTerm}
+//             onChange={handleSearch}
+//           />
+//           <TransactionList transactions={filteredTransactions} />
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+
+import Home from "./Home";
 import TransactionList from "./TransactionList";
 import AddTransactionForm from "./AddTransactionForm";
-import Home from "./Home";
 import { getTransactions } from "./Api";
 
 const App = () => {
@@ -25,15 +95,18 @@ const App = () => {
   }, []);
 
   const handleAddTransaction = (transaction) => {
-    setTransactions([...transactions, transaction]);
-    setFilteredTransactions([...filteredTransactions, transaction]);
+    setTransactions((transactions) => [...transactions, transaction]);
+    setFilteredTransactions((filteredTransactions) => [...filteredTransactions, transaction]);
   };
 
   const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+
     const filtered = transactions.filter((transaction) =>
-      transaction.description.toLowerCase().includes(event.target.value.toLowerCase())
+      transaction.description.toLowerCase().includes(searchTerm)
     );
+
     setFilteredTransactions(filtered);
   };
 
